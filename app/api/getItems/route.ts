6f17a8305c1cb10967c7e90b0cc45729be88c4f6
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-11-19 00:54:18 +1100

    edited cd file

--- app/api/getItems/route.ts
+++ app/api/getItems/route.ts
@@ -2,8 +2,8 @@
 import { db } from "@vercel/postgres"
 import { NextResponse } from "next/server"
 export async function GET() {
-  const client = await db.connect()
   try {
+    const client = await db.connect()
     const status = "listed"
     const res = await client.sql `SELECT * FROM items WHERE status=${status}`
     console.log(res.rows)
