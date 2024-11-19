30751ca332ce7424e0d2b76f6b2399c78f0ac6c5
Jeffrey Zhang <pengtuzhang@gmail.com>
2024-11-19 20:52:29 +1100

    Implemented middleware to guard pages and APIs, linted.

--- app/api/addItem/route.ts
+++ app/api/addItem/route.ts
@@ -26,8 +26,8 @@ export async function POST(req: NextRequest) {
   const s3_client = new S3Client({
     region: region,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ""
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "placeholder",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "placeholder"
     }
   })
 
